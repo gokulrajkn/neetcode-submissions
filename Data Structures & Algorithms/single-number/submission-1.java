e29634cc3454642a768b2690a25068b631555676
GokulRaj Nandakumar <38807518+gokulrajkn@users.noreply.github.com>
2026-08-17 19:30:28 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int temp = 0;
+        for(int num : nums) {
+            temp = temp ^ num;
+        }
+        return temp;
+    }
+}
